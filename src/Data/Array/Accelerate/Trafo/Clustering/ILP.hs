5f4498a25df6f8586dfdb076c441c213b97780ee
David <d.p.vanbalen@uu.nl>
2021-01-26 17:48:38 +0100

    some ILP work

--- src/Data/Array/Accelerate/Trafo/Clustering/ILP.hs
+++ src/Data/Array/Accelerate/Trafo/Clustering/ILP.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE KindSignatures, RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE KindSignatures #-}
 module Data.Array.Accelerate.Trafo.Clustering.ILP where
 import qualified Data.IntMap as M
 import Data.Array.Accelerate.AST.Operation (Execute)
@@ -9,6 +12,7 @@ import Data.Array.Accelerate.AST.Partitioned ( PreOpenAcc, Cluster )
 import Data.Array.Accelerate.AST.LeftHandSide ( Exists )
 
 import Data.Kind ( Type )
+
 
 -- In this file, order very often subly does matter.
 -- To keep this clear, we use S.Set whenever it does not,
@@ -22,6 +26,7 @@ import qualified Data.Set as S
 -- later, the interfaces are all quite similar
 import qualified Numeric.Limp.Program as P
 import qualified Numeric.Limp.Rep.IntDouble as R
+import Data.List (foldl')
 
 
 
@@ -30,12 +35,12 @@ import qualified Numeric.Limp.Rep.IntDouble as R
 -- identifies nodes with unique Ints
 type Label = Int
 
--- | Directed edge (a,b): `a` must be computed [strictly?] before `b`.
+-- | Directed edge (a,b): `a` must be computed [possibly strictly] before `b`.
 type Edge = (Label, Label)
 
-
+-- TODO: at some point, set fusibleEdges := fusibleEdges \\ infusibleEdges. Makes ILP smaller.
 data Graph = Graph
-  { nodes          :: S.Set Label
+  { graphNodes     :: S.Set Label
   , fusibleEdges   :: S.Set Edge  -- Can   be fused over, otherwise `a` before `b`.
   , infusibleEdges :: S.Set Edge  -- Can't be fused over, always    `a` before `b`.
   }
@@ -44,15 +49,26 @@ data Graph = Graph
 -- The graph is the 'common part' of the ILP,
 -- each backend has to encode their own constraints
 -- describing the fusion rules.
-data Information op = Information Graph (S.Set (Constraint op))
+data Information op = Information Graph (Constraint op) (S.Set (P.Bounds (Variable op) () R.IntDouble))
 
 
 class MakesILP op where
   -- Variables needed to express backend-specific fusion rules.
   type BackendVar op
-  
+
+  -- Control flow cannot be fused, so we make separate ILPs for e.g.
+  -- then-branch and else-branch. In the future, a possible optimisation is to 
+  -- generate code for the awhile-condition twice: once maybe fused after the body, 
+  -- once maybe fused after input. For now, condition and body are both treated
+  -- as 'foreign calls', like if-then-else.
+  -- The IntMap maps from a label to the corresponding ILP (only for things treated
+  -- as 'foreign calls', like control flow).
+  -- A program can result in multiple ILPs, for example, the body of an 'awhile' cannot be fused with anything outside of it.
+  -- you `can` encode this in the ILP, but since the solutions are independant of each other it should be much more efficient
+  -- to solve them separately. This avoids many 'infusible edges', and significantly reduces the search space. The extracted
+  -- subtree gets encoded as a sort of 'foreign call', preventing all fusion.
   -- todo: maybe we can extract more commonality from this, making the class more complicated but instances smaller/easier
-  mkGraph :: PreOpenAcc (Execute op) () a-> Information op
+  mkGraph :: PreOpenAcc (Execute op) () a-> (Information op, M.IntMap (Information op))
 
   -- for efficient reconstruction - important that the numbers match the numbers in mkGraph. Can fuse the two to ensure,
   -- or do a separate 'labelling' pass first (:: Execute op -> LabelledExe op).
@@ -77,31 +93,45 @@ type Constraint op = P.Constraint (Variable   op) () R.IntDouble
 -- todo: not all Idxs need to have the same 'a', so `list` should be `hlist` or tuplist :)
 data UnsafeConstruction (op :: Type -> Type) = forall a. UnsafeConstruction (S.Set Int) (forall env. [Idx a env] -> Execute op env)
 
--- A program can result in multiple ILPs, for example, the body of an 'awhile' cannot be fused with anything outside of it.
--- you `can` encode this in the ILP, but since the solutions are independant of each other it should be much more efficient
--- to solve them separately. This avoids many 'infusible edges', and significantly reduces the search space. The extracted
--- subtree gets encoded as a sort of 'foreign call', preventing all fusion.
-makeILPs :: Information op -> (ILP op, M.IntMap (ILP op))
-makeILPs (Information graph backendcons) = somehowCombine graphILPs backendcons
+
+makeILP :: Information op -> ILP op
+makeILP  (Information 
+            (Graph nodes fuseEdges nofuseEdges)
+            backendconstraints
+            backendbounds
+          ) = combine graphILP
   where
-    somehowCombine = undefined -- Put all backend-specific constraints into each ILP? Or can we be smarter,
-                               -- only adding ones that somehow 'matter' for the ILPs? Is the solver smart
-                               -- enough, and will it disregard these other constraints? What if the backend
-                               -- somehow needs the values that the backend-specific variables take in the
-                               -- solution, then we need to find out which solution is the relevant one?
-    graphILPs :: forall op. (ILP op, M.IntMap (ILP op))
-    graphILPs = undefined -- Formula's from the papers, such that:
-                          --  - clusters are acyclic (w.r.t. infusible edges and non-fused fusible edges)
-                          --  - infusible edges are respected
-                          --  - number of clusters is minimised
-                          -- 
-                          -- New stuff: control flow cannot be fused, so we make separate ILPs for e.g.
-                          -- then-branch and else-branch. In the future, a possible optimisation is to 
-                          -- generate code for the awhile-condition twice: once maybe fused after the body, 
-                          -- once maybe fused after input. For now, condition and body are both treated
-                          -- as 'foreign calls', like if-then-else.
-                          -- The IntMap maps from a label to the corresponding ILP (only for things treated
-                          -- as 'foreign calls', like control flow).
+    combine (P.Program dir fun cons bnds) = 
+             P.Program dir fun (cons <> backendconstraints) 
+                               (bnds <> S.toList backendbounds) 
+    
+    n = S.size nodes
+    --                                  __ | don't know why the objFun has to be
+    --                                 /   | 'real', everything else is integral 
+    --                                 v
+    graphILP = P.Program P.Minimise (P.toR objFun) constraints bounds
+
+    -- placeholder, currently maximising the number of vertical/diagonal fusions.    
+    objFun = foldl' (\f (i, j) -> f P..+. P.z1 (Fused i j)) P.c0 (S.toList fuseEdges)
+
+    constraints = acyclic <> infusible
+
+    -- x_ij <= pi_j - pi_i <= n*x_ij for all fusible edges
+    acyclic = foldMap (\(i, j) -> P.Between 
+                                    (P.z1 (Fused i j))
+                                    (P.z1 (Pi j) P..-. P.z1 (Pi i))
+                                    (P.z  (Fused i j) (fromIntegral n))) fuseEdges
+
+    -- pi_j - pi_i >= 1 for all infusible edges (i,j)
+    infusible = foldMap (\(i, j) -> (P.z1 (Pi j) P..-. P.z1 (Pi i)) P.:>= P.c1) nofuseEdges
+
+    bounds = map (\i      -> P.lowerUpperZ 0 (Pi i) (fromIntegral n)) 
+                 (S.toList nodes)
+          ++ map (\(i, j) -> P.binary (Fused i j)) 
+                 (S.toList fuseEdges)
+
+    
+
                       
 
 -- call the solver, and interpret the result as a list (in order of execution) of clusters (sets of nodes).
